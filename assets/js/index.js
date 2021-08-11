ad16737c614a67c5836958cb5859056a2e4ce27b
leezon <1603963868@qq.com>
2021-08-11 09:21:50 +0800

    update

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+let root = document.getElementById('root');
+
+let setScale = debounce(() => {
+  let scale = getScale();
+  root.style.transform = `scale(${scale})`;
+}, 200);
+
+window.addEventListener('resize', setScale);
+setScale();
+
+function getScale(width = 1920, height = 1080) {
+  let scaleW = window.innerWidth / width;
+  let scaleH = window.innerHeight / height;
+  return scaleW < scaleH ? scaleW : scaleH;
+}
+
+function debounce(fn, wait = 200) {
+  let timeout = null;
+  return function () {
+    if (timeout !== null) clearTimeout(timeout);
+    timeout = setTimeout(fn, wait);
+  };
+}
